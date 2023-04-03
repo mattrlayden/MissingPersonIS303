941610ce5531fa8f85f5998ade778d7ca58ef2b6
mattrlayden <matt.r.layden@gmail.com>
2023-04-03 14:19:08 -0600

    commit changes

--- Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/urls.py
+++ Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import indexPageView
+from .views import aboutPageView
+from .views import MissingpeoplePageView
+from .views import ContactPageView
 
 urlpatterns = [
-     path("", indexPageView, name="index")
+     path("", indexPageView, name="index"),
+     path("about/", aboutPageView, name = "about"),
+     path("index/", indexPageView, name="index"),
+     path("missingpeople/", MissingpeoplePageView, name="missingpeople"),
+     path("contact/", ContactPageView, name="contact")
 ]
--- Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/views.py
+++ Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+# Create your views here.
 def indexPageView(request):
     return render(request, 'trafficapp/index.html')
-# Create your views here.
+
+def aboutPageView(request):
+    return render(request, 'trafficapp/about.html')
+
+def MissingpeoplePageView(request):
+    return render(request, 'trafficapp/missingpeople.html')
+
+def ContactPageView(request):
+    return render(request, 'trafficapp/contact.html')

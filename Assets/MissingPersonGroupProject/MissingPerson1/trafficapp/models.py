9232d352d30d2da9e3feccd19b76a63ef503b897
mattrlayden <matt.r.layden@gmail.com>
2023-04-07 14:02:32 -0600

    commit changes

--- Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/models.py
+++ Assets/MissingPersonGroupProject/MissingPerson1/trafficapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Person(models.Model):
+    date_missing = models.DateField(default=datetime.today, blank=True)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    age_at_missing = models.IntegerField(default=0)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    gender = models.CharField(max_length=100)    
+    race = models.CharField(max_length=13, blank=True)
+
+    def __str__(self):
+        return (self.full_name)
+    
+    @property
+    def full_name(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    #override the save method
+    def save(self):
+        self.first_name = self.first_name.upper()
+        self.last_name = self.last_name.upper()
+        super(Person, self).save() # Call the "real" save() method.                                
